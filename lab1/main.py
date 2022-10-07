e0f72dccf4956dc59ffcb892b2fe6d72c45e2933
wibox <francesco.info.pagano@gmail.com>
2022-10-07 14:03:55 +0200

    test

--- lab1/main.py
+++ lab1/main.py
@@ -3,6 +3,11 @@ from numpy import random
 SIMULATION_TIME = 1000000
 current_time = 0
 
+SERVERS = 3
+
+server_idle = "idle"
+server_busy = "busy"
+
 def generate_arrival_time(current_time):
     return current_time + random.exponential()
 
@@ -24,7 +29,12 @@ class Server():
 class Queue():
     def __init__(self, FES):
         self.FES = sorted(FES, key=lambda x: x.get_arrival_time(), reversed=True)
+    
+    def serve_next_client(self):
+        pass
 
+
+servers = [Server(server_idle) for i in range(SERVERS)]
 
 if __name__ == "__main__":
     while current_time < SIMULATION_TIME:
