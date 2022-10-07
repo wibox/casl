4eaf2cf6f67024d7b480367837957cc8e406ce34
wibox <francesco.info.pagano@gmail.com>
2022-10-07 13:45:35 +0200

    structure improved

--- lab1/main.py
+++ lab1/main.py
@@ -3,20 +3,29 @@ from numpy import random
 SIMULATION_TIME = 1000000
 current_time = 0
 
-def generate_arrival_time():
-    pass
+def generate_arrival_time(current_time):
+    return current_time + random.exponential()
 
-def generate_service_time():
-    pass
+def generate_service_time(current_time):
+    return current_time + random.exponential()
 
 class Client():
-    pass
+    def __init__(self, arrival_time, type):
+        self.arrival_time = arrival_time
+        self.type = type
+
+    def get_arrival_time(self):
+        return self.arrival_time
 
 class Server():
-    pass
+    def __init__(self, status):
+        self.status = status
 
 class Queue():
-    pass
+    def __init__(self, FES):
+        self.FES = sorted(FES, key=lambda x: x.get_arrival_time(), reversed=True)
+
 
 if __name__ == "__main__":
-    while current_time < SIMULATION_TIME
+    while current_time < SIMULATION_TIME:
+        print("Ciao!")
