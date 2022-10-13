4680a0e876afa8dfcb9012b59d5d3313294eb269
wibox <francesco.inf.pagano@gmail.com>
2022-10-13 10:23:08 +0200

    simulator completed

--- lab1/main.py
+++ lab1/main.py
@@ -1,85 +1,70 @@
-from numpy import random
+import numpy as np
 
-from clock_random_gen import SIMULATION_TIME
+np.random.seed(123456789)
 
-#paramenters
+current_time = 0
+SIMULATION_TIME = 100000
+
 LAMBDA = 3
 MU = 4
-SIMULATION_TIME = 1000
 
-server_busy = "busy"
-server_idle = "idle"
-client_arrival = "arrival"
-client_departure = "departure"
+customers = 0
 
-client = 0
+queue = list()
+FES = list()
 
-#main functions for generating random arrival and service times
-def generate_arrival_time(current_time):
-    return current_time + random.exponential(1/LAMBDA)
+def generate_arrival_time():
+    return np.random.exponential(1/MU)
 
 def generate_service_time():
-    return random.exponential(1/MU)
+    return np.random.exponential(1/LAMBDA)
 
-#main classes of the system
-class Queue():
-    def __init__(self, FES, server):
-        self.FES = FES
-        self.server = server
-        self.customers_in_queue = 0
-        self.current_time = 0
+def arrival(time, queue, FES, logger):
+    global customers
+    inter_arrival = generate_arrival_time()
+    FES.append((time+inter_arrival, "arrival"))
+    customers = customers + 1
+    customer = Customer(type="arrival", arrival_time=time)
+    queue.append(customer)
+    logger.log_arrival(time+inter_arrival, customers)
+    service_time = generate_service_time()
+    FES.append((time+service_time, "departure"))
 
-    def add_arrival(self, client):
-        self.FES = sorted(self.FES.append(client), key=lambda x : x.get_arrival_time())
-
-    def set_departure(self):
-        return self.FES.pop(0)
-
-    def get_next_event(self):
-        return self.FES[0]
-
-    def change_server_status(self, status):
-        self.server.status = status
-
-    def set_time(self, increase=1):
-        self.current_time = self.current_time + increase
-
-class Server():
-    def __init__(self, status):
-        self.status = status
-
-class Client():
-    def __init__(self, arrival_time, type):
-        self.arrival_time = arrival_time
-        self.type = type
-
-    def get_arrival_time(self):
-        return self.arrival_time
-
-    def set_type(self, type):
-        self.type = type
+def departure(time, queue, FES, logger):
+    global customers
+    customer = queue.pop(0)
+    customers = customers - 1
+    if customers > 0:
+        service_time = generate_service_time()
+        FES.append((time+service_time, "departure"))
+        logger.log_departure(time+service_time, customers)
 
 class Logger():
-    def __init__(self):
-        pass
+    def log_departure(self, time, queue_length):
+        print(f"Departure at time: {time}. Current Queue length: {queue_length}.")
 
-FES = list()
-myServer = Server(server_idle)
-myQueue = Queue(FES, myServer)
+    def log_arrival(self, time, queue_length):
+        print(f"Arrival scheduled at time: {time}. Current Queue length: {queue_length}.")
 
-if __name__ == "__main__":
-    #generate first event of type arrival
-    # if(myQueue.server.status == server_idle):
-    #     newClient = Client(generate_arrival_time(myQueue.current_time), "arrival")
-    #     #schedule its service time making server busy
-    #     service_time = generate_service_time(myQueue.current_time)
-    #     myQueue.change_server_status(server_busy)
-    #     myQueue.set_time(myQueue.current_time + service_time)
-    #     myQueue.set_departure()
-    #     newClient.set_type(client_departure)
-    #main even loop
-    while myQueue.current_time < SIMULATION_TIME:
-        print(myQueue.current_time)
-        service_time = generate_service_time()
-        print(f"service time: {service_time}")
-        myQueue.set_time(myQueue.current_time + service_time)
+class Customer():
+    def __init__(self, type, arrival_time):
+        self.type = type
+        self.arrival_time = arrival_time
+
+if __name__ == '__main__':
+    #main event loop
+    FES.append((0, "arrival"))
+    queue.append(Customer(type="arrival", arrival_time=0))
+    myLogger = Logger()
+    while(current_time < SIMULATION_TIME) or (not FES):
+        time, event_type = FES.pop(0)
+        current_time = time
+        if len(queue) > 0:
+            if event_type == "arrival":
+                arrival(current_time, queue, FES, myLogger)
+            elif event_type == "departure":
+                departure(current_time, queue, FES, myLogger)
+        else:
+            print("Queue is empty. Terminating.")
+            break
+        FES = sorted(FES, key=lambda x : x[0])
