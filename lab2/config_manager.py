cf71c245642db1f061f00e93be760314a54fe067
wibox <francesco.inf.pagano@gmail.com>
2022-10-23 18:34:46 +0200

    reverting refactoring changes

--- lab2/config_manager.py
+++ lab2/config_manager.py
@@ -58,7 +58,7 @@ myConfigSetter.write_config(configs=ParameterGrid(PARAMETERS))
 myConfigGetter = ConfigGetter(config_path=CONFIG_PATH, config_file=CONFIG_FILE)
 test_configurations = myConfigGetter.retrieve_configs()
 
-with open("execute.sh", "w") as f:
+with open("commons/execute.sh", "w") as f:
     print(f"Writing {len(test_configurations)} configuratons to {myConfigGetter.path}/{myConfigGetter.config_file}")
     for configuration in test_configurations:
         f.write(f"python3 main.py --area {configuration['area']} --speed {configuration['speed']} --players {configuration['players']} --seed {configuration['seed']} \n")
--- lab2/logger.py
+++ lab2/logger.py
@@ -16,28 +16,28 @@ class Logger():
 
     def write_header(self, header, filename):
         try:
-            with open(f"../{self.log_path}/{filename}", "w") as f:
+            with open(f"{self.log_path}/{filename}", "w") as f:
                 f.write(header)
         except:
             print(f"Error dealing with log file at {self.log_path}/{filename}")
 
     def log_time(self, filename, time, other):
         try:
-            with open(f"../{self.log_path}/{filename}", "a") as f:
+            with open(f"{self.log_path}/{filename}", "a") as f:
                 f.write(f"{time},{other}\n")
         except:
             print(f"Error dealing with time_log file at {self.log_path}/{filename}")
 
     def log_winner_kills(self, filename, kills, other):
         try:
-            with open(f"../{self.log_path}/{filename}", "a") as f:
+            with open(f"{self.log_path}/{filename}", "a") as f:
                 f.write(f"{kills},{other}\n")
         except:
             print(f"Error dealing with kills_log file at {self.log_path}/{filename}")
 
     def log_average(self, filename, avg, other):
         try:
-            with open(f"../{self.log_path}/{filename}", "a") as f:
+            with open(f"{self.log_path}/{filename}", "a") as f:
                 f.write(f"{avg},{other}\n")
         except:
             print(f"Error dealing with avg_log file at {self.log_path}/{filename}")
