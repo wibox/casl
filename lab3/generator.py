2535445e50b629b7cc939efa1cef0dcb41aea214
wibox <francesco.inf.pagano@gmail.com>
2022-11-01 16:55:37 +0100

    assignment completed

--- lab3/generator.py
+++ lab3/generator.py
@@ -1,7 +1,16 @@
 import numpy as np
 import scipy.special as ss
+from scipy.stats import binom
 import matplotlib.pyplot as plt
 import time
+
+def plot_pdf(x, pdf):
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.plot(x, pdf, color="red")
+    ax.set_xlabel("Points")
+    ax.set_ylabel("Density")
+    plt.suptitle("Normal PDF")
+    plt.show()
 
 def plot_rv(results, probs, num_istances):
     fig, ax = plt.subplots(1, 2, figsize=(5,5))
@@ -19,6 +28,20 @@ def plot_rv(results, probs, num_istances):
     ax[1].set_xlabel("Realizations")
     plt.suptitle("Binomial generation through convolution")
     plt.show()
+
+def log_exp_factorial(n):
+    fact = 0
+    for nn in range(n):
+        fact += np.log(nn+1)
+    
+    return np.exp(fact)
+
+def binomial_coeff(n, k):
+    """
+    This function computes the binomial coefficient using log (multiplications become sums) and then exp to get back
+    the original result. The actual method that performs the logtrick is log_exp_factorial.
+    """
+    return log_exp_factorial(n)/((log_exp_factorial(k))*(log_exp_factorial(n-k)))
 
 def binomial_convolution(n, p, num_istances, plot_pmf_probs):
     """
@@ -50,9 +73,9 @@ def binomial_inverse(n, p, num_istances, plot_pmf_probs):
     """
     start_time = time.time()
     #probabilities of k positive outcomes
-    # probs_success = [ss.binom(n, success)*(p**success)*((1-p)**(n-success)) for success in range(n)]
-    log_probs_success = [ss.binom(n, success) + success*np.log(p) + (n-success)*np.log(1-p) for success in range(n)]
-    probs_success = [np.exp(prob) for prob in log_probs_success]
+    probs_success = [binomial_coeff(n, success)*(p**success)*((1-p)**(n-success)) for success in range(n)]
+    # log_probs_success = [ss.binom(n, success) + success*np.log(p) + (n-success)*np.log(1-p) for success in range(n)]
+    # probs_success = [np.exp(prob) for prob in log_probs_success]
     #number of positive outcomes
     positive_outcomes = [i for i in range(n)]
     results = []
@@ -68,6 +91,27 @@ def binomial_inverse(n, p, num_istances, plot_pmf_probs):
                     results.append(positive_outcomes[idx-1])
                 idx = idx + 1
 
+    if plot_pmf_probs:
+        plot_rv(results, probs_success, num_istances)
+
+    return results, probs_success, time.time()-start_time
+
+def binomial_inverse_no_of(n, p, num_istances, plot_pmf_probs):
+    """
+    This function uses scipy.stats.binom to generate the cdf instead of building it manually.
+    """
+    start_time = time.time()
+    probs_success = [binom.cdf(success, n, p) for success in range(1,n)]
+
+    u = np.random.uniform(low=0, high=1)
+
+    results = []
+
+    for take in range(num_istances):
+        for x in range(n-1):
+            if u >= probs_success[x] and u <= probs_success[x+1]:
+                results.append(x)
+    
     if plot_pmf_probs:
         plot_rv(results, probs_success, num_istances)
 
@@ -100,8 +144,72 @@ def binomial_empirical(n, p, num_istances, plot_pmf_probs):
 
     return results, probs, time.time()-start_time
 
-def normal_acceptance():
-    pass
+def normal_dist(x, mean, s):
+    return 1/np.sqrt(2*np.pi*s) * np.exp(-0.5*((x-mean)/s)**2)
 
-def rice():
-    pass
+def normal_acceptance(num_points, mean, s, a, b, num_istances, plot_pdf):
+    """
+    This function generates an istance of a normal random variable according to the acceptance/rejecton method.
+    """
+    start_time = time.time()
+    space = np.linspace(a, b, num_points)
+    dist = normal_dist(space, mean, s)
+
+    results = []
+
+    c = np.max(dist)
+    for _ in range(num_istances):
+        found = False
+        if not found:
+            x = np.random.uniform(low=a, high=b)
+            y = np.random.uniform(low=0, high=c)
+            if y <= normal_dist(x, mean, s):
+                found = True
+                results.append(y)
+
+    if plot_pdf:
+        plot_pdf(space, dist)
+
+    if num_istances == 1:
+        return results, dist, start_time-time.time()
+    else:
+        return results[0], dist, start_time-time.time()
+
+def poisson(l):
+    """
+    This function generates an istance of a poisson distributed random variable with parameter l using Knuth algorithm.
+    """
+    n, q = 0, 1
+    found = False
+    while not found:
+        u = np.random.uniform(low=0, high=1)
+        q = q*u
+        if q < np.exp(-l):
+            found=True
+            return n
+        else:
+            n += 1
+    return 0
+
+def chisquared(n):
+    """
+    This function generated an istance of a chisquared-distrubted random variable with parameter n using convolution method.
+    """
+    normals = [np.random.normal(loc=0, scale=1) for _ in range(n)]
+    return sum(normals)
+
+def rice(n, s):
+    """
+    This function generates a random variable distributed according to the Rice distribution X ~ R(n, s).
+    Strategy:
+        - Generate N ~ Poisson(k) with k = n^2 / 2s^2
+        - Generate W ~ ChiSquared(2N+2)
+        - Then R = s*sqrt(W) is rician-distributed.
+    """
+    #Generating poisson distributed r.v.
+    l = n**2/(2*s**2)
+    N = poisson(l)
+    #Generating chisquared distributed r.v.
+    W = chisquared(2*N+2)
+    #Generating Rice.
+    return s*np.sqrt(W)
--- lab3/main.py
+++ lab3/main.py
@@ -2,7 +2,7 @@ import generator as g
 import pandas as pd
 
 NS = [10, 100, int(1e6)]
-PROBABILITIES = [0.5, 0.01, 0.00001]
+PROBABILITIES = [0.5, 0.01, 1e-5]
 NUM_ISTANCES = 50
 
 HEADER="method,n,p,time\n"
@@ -11,12 +11,14 @@ if __name__ == "__main__":
 
     with open("log.csv", "w") as f:
         f.write(HEADER)
-    with open("log.csv", "a") as flog:
+    with open("log.csv", "a") as f_log:
         for n, p in zip(NS, PROBABILITIES):
             results_conv, probs_conv, time_conv = g.binomial_convolution(n=n, p=p, num_istances=NUM_ISTANCES, plot_pmf_probs=False)
-            flog.write(f"'conv',{n},{p},{time_conv}\n")
-            results_inv, probs_inv, time_inv = g.binomial_inverse(n=n, p=p, num_istances=NUM_ISTANCES, plot_pmf_probs=False)
-            flog.write(f"'inv',{n},{p},{time_inv}\n")
+            f_log.write(f"'conv',{n},{p},{time_conv}\n")
+            results_inv, probs_inv, time_inv = g.binomial_inverse_no_of(n=n, p=p, num_istances=NUM_ISTANCES, plot_pmf_probs=False)
+            f_log.write(f"'inv',{n},{p},{time_inv}\n")
             results_exp, probs_exp, time_exp = g.binomial_empirical(n=n, p=p, num_istances=NUM_ISTANCES, plot_pmf_probs=False)
-            flog.write(f"'exp',{n},{p},{time_exp}\n")
-
+            f_log.write(f"'exp',{n},{p},{time_exp}\n")
+        
+    print(f"Random Normal R.V. with acceptance/rejection method: {g.normal_acceptance(num_points=100, mean=0, s=1, a=-1, b=1, num_istances=1, plot_pdf=False)[0][0]}")
+    print(f"Random Rician-distributed R.V. exploiting relation with Poisson and ChiSquared distribution: {g.rice(5, 3)}")
