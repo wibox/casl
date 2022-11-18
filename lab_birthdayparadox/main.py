a10d5fb323c1446137f352d3e7a2ab6c89384a65
wibox <francesco.info.pagano@gmail.com>
2022-11-18 13:13:49 +0100

    reformatted

--- lab_birthdayparadox/main.py
+++ lab_birthdayparadox/main.py
@@ -13,26 +13,12 @@ args = parser.parse_args()
 SEED = 299266
 np.random.seed(SEED)
 
-INSTANCES = [i for i  in range(10, 110, 10)]
+INSTANCES = [i for i  in range(10, 110, 10)]    
 
 def main():
-
-    conflicts = list()
-    lengths = list()
-    probs = list()
-    counter = 0
     
-    for instances in INSTANCES:
-        new_set = generate_uniform_set(m=instances, upper_bound=args.prop_upper_bound)
-        conflict, local_counter = check_for_conflicts(test_set=new_set, upper_bound=args.prop_upper_bound)
-        counter += local_counter
-        if conflict:
-            conflicts.append(len(new_set))
-        lengths.append(len(new_set))
-        probs.append(counter/len(INSTANCES))
-    print(f"Average: {np.mean(conflicts)}")
-    plot_result(title="uniform", filepath="results/", filename="uniform_test.svg", events=lengths, probs=probs)
-
+    uniform_analysis(cardinalities=INSTANCES, upper_bound=args.prop_upper_bound)
+    # real_analysis()
 
 if __name__ == "__main__":
     main()
--- lab_birthdayparadox/utils.py
+++ lab_birthdayparadox/utils.py
@@ -26,17 +26,18 @@ def generate_uniform_set(m : int, upper_bound : int) -> List[Obj]:
     This function returns a list of object with a given property. According to the problem's specification
     this is just the id of the day of the year corresponding to the person's birthday.
     """
-    return [Obj(id=i,prop=Property(upper_bound=365).generate_uniform_prop()).prop for i in range(m)]
+    return [Obj(id=i,prop=Property(upper_bound=upper_bound).generate_uniform_prop()).prop for i in range(m)]
 
-def check_for_conflicts(test_set : List[Obj], upper_bound : int) -> Tuple[bool, int]:
-    counter = 0
+def generate_real_set(m : int) -> List[Obj]:
+    pass
+
+def check_for_conflicts(test_set : List[Obj], upper_bound : int) -> bool:
     lookup_table = [0 for _ in range(1, upper_bound + 1)]
     for prop in test_set:
         lookup_table[prop] += 1
         if lookup_table[prop] > 1:
-            counter += 1
-            return True, counter
-    return False, counter
+            return True
+    return False
 
 def plot_result(title : str, filepath : str, filename : str, events : List[int], probs : List[int], savefig_bool : bool = True) -> None:
     fig, ax = plt.subplots(figsize=(10, 15))
@@ -47,3 +48,27 @@ def plot_result(title : str, filepath : str, filename : str, events : List[int],
     plt.grid()
     plt.show()
     plt.savefig(os.path.join(filepath, filename))
+
+def uniform_analysis(cardinalities : List[int], upper_bound : int):
+    counter = 0
+    conflicts = list()
+    lengths = list()
+    probs = list()
+    # generate uniformly distributed set for each cardiality
+    for cardinality in cardinalities:
+        new_set = generate_uniform_set(m=cardinality, upper_bound=upper_bound)
+        # check if a conflict is experienced
+        conflict = check_for_conflicts(test_set=new_set, upper_bound=upper_bound)
+        if conflict:
+            counter += 1
+            conflicts.append(len(new_set))
+        lengths.append(len(new_set))
+        probs.append(counter / len(cardinalities))
+    # compute average number of objects to experience conflict
+    print(f"Average: {np.mean(conflicts)}")
+    # estimate probability
+    plot_result(title="uniform", filepath="results/", filename="uniform.png", events=lengths, probs=probs)
+    pass
+
+def real_analysis():
+    pass
