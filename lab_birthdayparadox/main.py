a9b8d15bc0b71649b92e21dffb2057590c37f87b
wibox <francesco.info.pagano@gmail.com>
2022-11-19 13:01:02 +0100

    avg missing

--- lab_birthdayparadox/main.py
+++ lab_birthdayparadox/main.py
@@ -5,10 +5,12 @@ import numpy as np
 from utils import *
 
 from tabulate import tabulate
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--prop-upper-bound", type=int, help="Upper bound for the generation of the desired property.", default=365)
 parser.add_argument("--num-instances", type=int, help="Maximum objects' set's cardinality.", default=110)
+parser.add_argument("--real-data", type=str, help="Filename corresponding to the real distribution of birthdays.", default="US_births_2000-2014_SSA.csv")
 args = parser.parse_args()
 
 # setting a single seed since each run is independent from the other
@@ -21,35 +23,52 @@ DIST_TYPEs = ["uniform", "real"]
 
 def main():
     # for each kind of property distribution
+    avg_dict = dict()
     for dist_type in DIST_TYPEs:
-        print(f"Using {dist_type} to generate property")
+        print(f"Using {dist_type} distribution to generate property")
         # for each cardinality of the set under study
         global_lengths = list()
         global_probs = list()
+
+        # study the average number of objects to experience a conflict w.r.t. their property
+        avg_cardinality = retrieve_avg_cardinality()
+        avg_ci = generate_avg_ci(cardinalities=INSTANCES)
+
+        avg_dict[f"{dist_type}"] = [avg_cardinality, avg_ci]
+
         for cardinality in INSTANCES:
             print(f"\tWorking with cardinality: {cardinality}. Performing {EXP_PER_INSTANCE} experiments")
-            # study the average number of objects to experience a conflict w.r.t. their property
-
             # study the probability of observing a conflict
             local_probs = list()
             counter = 0
-            for _ in range(EXP_PER_INSTANCE):
+            for _ in tqdm(range(EXP_PER_INSTANCE)):
+
                 if dist_type=="uniform":
-                    new_set = generate_uniform_set(m=cardinality, upper_bound=args.prop_upper_bound)
+                    new_set = generate_uniform_set(m=cardinality, prop_upper_bound=args.prop_upper_bound)
                 else:
-                    new_set = generate_real_set()
-                if check_for_conflicts(test_set=new_set, upper_bound=args.prop_upper_bound):
+                    probs = generate_real_probs(filename=args.real_data)
+                    new_set = generate_real_set(m=cardinality, prop_upper_bound=args.prop_upper_bound, probs=probs)
+
+                if check_for_conflicts(test_set=new_set, prop_upper_bound=args.prop_upper_bound):
                     counter += 1
+
             local_probs.append(counter/EXP_PER_INSTANCE)
             global_probs.append(np.mean(local_probs))
             global_lengths.append(len(new_set))
+            # computing confidence interval for global_probs
+            upper_bound, lower_bound = generate_prob_ci(probs=global_probs, experiments=EXP_PER_INSTANCE)
+            # building theoreical result
+            theo_prob = theoretical_prob(m=global_lengths, prop_upper_bound=args.prop_upper_bound)
 
         plot_result(title=f"Conflicts happening with {dist_type} distribution for property",
                     filepath="results/",
-                    filename=f"{dist_type}.png",
+                    filename=f"{dist_type}",
                     events=global_lengths,
-                    probs=global_probs)
+                    exp_probs=global_probs,
+                    upper_probs=upper_bound,
+                    lower_probs=lower_bound,
+                    theo_probs=theo_prob,
+                    savefig_bool=True)
                 
-
 if __name__ == "__main__":
     main()
--- lab_birthdayparadox/utils.py
+++ lab_birthdayparadox/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as st
 
 from typing import *
 import os
@@ -7,44 +9,112 @@ import os
 class Property():
     def __init__(self, upper_bound):
         self.upper_bound = upper_bound
-        self.prop = 0
+        self.uniform_prop = 0
+        self.real_prop = 0
 
     def generate_uniform_prop(self) -> int:
-        self.prop = np.random.randint(low=1,high=self.upper_bound)
-        return self.prop
+        self.uniform_prop = np.random.randint(low=1,high=self.upper_bound)
+        return self.uniform_prop
+
+    def generate_real_prop(self, probs : List[float]) -> int:
+        self.real_prop = np.random.choice(a=np.array(range(self.upper_bound)), p=probs)
+        return self.real_prop
 
 class Obj():
-    def __init__(self, id, prop):
+    def __init__(self, id, uniform_prop=None, real_prop=None):
         self.id = id
-        self.prop = prop
+        self.uniform_prop = uniform_prop
+        self.real_prop = real_prop
 
     def __str__(self) -> str:
-        return f"Object: {self.id} -> {self.prop}"
+        return f"Object: {self.id} -> U:{self.uniform_prop} -> R:{self.real_prop}"
 
-def generate_uniform_set(m : int, upper_bound : int) -> List[Obj]:
-    """
-    This function returns a list of object with a given property. According to the problem's specification
-    this is just the id of the day of the year corresponding to the person's birthday.
-    """
-    return [Obj(id=i,prop=Property(upper_bound=upper_bound).generate_uniform_prop()).prop for i in range(m)]
-
-def generate_real_set(m : int) -> List[Obj]:
+def retrieve_avg_cardinality(experiments : int, dist_type : str) -> float:
     pass
 
-def check_for_conflicts(test_set : List[Obj], upper_bound : int) -> bool:
-    lookup_table = [0 for _ in range(1, upper_bound + 1)]
+def generate_uniform_set(m : int, prop_upper_bound : int) -> List[Obj]:
+    """
+    This function returns a list of object with a given property. According to the problem's specification
+    this is just the id of the day of the year corresponding to the person's birthday. The underlying distribution
+    is Uniform.
+    """
+    return [Obj(id=i,uniform_prop=Property(upper_bound=prop_upper_bound).generate_uniform_prop()).uniform_prop for i in range(m)]
+
+def generate_real_set(m : int, prop_upper_bound : int, probs : List[float]) -> List[Obj]:
+    """
+    This function returns a list of objects with a given property. According to the probem's specification
+    this is just the id of the day of the year corresponding to the person's birthday. The underlying distribution
+    is retrieved from real data.
+    """
+    return [Obj(id=i,real_prop=Property(upper_bound=prop_upper_bound).generate_real_prop(probs=probs)).real_prop for i in range(m)]
+
+def generate_uniform_instance(prop_upper_bound : int) -> Obj:
+    pass
+
+def generate_real_probs(filename: str) -> List[float]:
+
+    b = pd.read_csv(filename)
+    b = b.groupby(['month','date_of_month'])
+    total_day_per_month = b['births'].sum()
+    # removing leap year hor homogeneity w.r.t. the rest of the code
+    total_day_per_month = total_day_per_month.drop((2,29))
+    total = total_day_per_month.sum() 
+    probs = (total_day_per_month/total).values
+
+    return probs
+
+def generate_real_instance() -> Obj:
+    pass
+
+def check_for_conflicts(test_set : List[Obj], prop_upper_bound : int) -> bool:
+    lookup_table = [0 for _ in range(1, prop_upper_bound + 1)]
     for prop in test_set:
         lookup_table[prop] += 1
         if lookup_table[prop] > 1:
             return True
     return False
 
-def plot_result(title : str, filepath : str, filename : str, events : List[int], probs : List[int], savefig_bool : bool = True) -> None:
+def generate_prob_ci(probs : List[float], experiments : int) -> Tuple[List[float], List[float]]:
+    upper_bound = list()
+    lower_bound = list()
+
+    cl = st.norm(loc=0, scale=1).ppf(.95)
+    for prob in probs:
+        s_hat = np.sqrt((prob*(1-prob))/experiments)
+        interval = cl*s_hat
+        upper_bound.append(prob+interval)
+        lower_bound.append(prob-interval)
+    
+    return upper_bound, lower_bound
+
+def generate_avg_ci(cardinalities : List[int]) -> int:
+    ci = st.t.interval(alpha=0.05, df=len(cardinalities)-1, loc=np.mean(cardinalities), scale=np.var(cardinalities))
+    return ci
+
+def theoretical_prob(m : List[int], prop_upper_bound : int) -> List[float]:
+    theo_prob = list()
+    for cardinality in m:
+        theo_prob.append(round(1 - pow(np.e, -1*(pow(cardinality,2))/(2*prop_upper_bound)), 5))
+    return theo_prob
+
+def plot_result(title : str,
+                filepath : str,
+                filename : str,
+                events : List[int],
+                exp_probs : List[float],
+                upper_probs : List[float],
+                lower_probs : List[float],
+                theo_probs : List[float],
+                savefig_bool : bool = True) -> None:
+
     fig, ax = plt.subplots(figsize=(10, 15))
-    ax.plot(events, probs)
+    ax.plot(events, exp_probs, label="empirical probability")
+    ax.plot(events, theo_probs, label="theoretical probability")
+    ax.fill_between(events, lower_probs, upper_probs, alpha=.5)
     ax.set_xlabel("Size of instance")
     ax.set_ylabel("Probability to experience a conflict")
     plt.title(title)
     plt.grid()
+    plt.legend()
     plt.show()
-    plt.savefig(os.path.join(filepath, filename))
+    fig.savefig(os.path.join(filepath, filename))
